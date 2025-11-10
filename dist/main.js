9468598302ae98681b3b75e4555514cbf74ccfbd
Medok Jusus <jesusmedok@gmail.com>
2025-11-10 18:06:53 +0300

    Mini-Bank

--- dist/main.js
+++
@@ -1,5 +0,0 @@
-import { Deposit } from "./AddFunds.js";
-import { WithdrawAmount } from "./WithdrawFunds.js";
-import { СheckBalance } from "./CheckBalance.js";
-import { clearTransaction } from "./ClearTransactions.js";
-//# sourceMappingURL=main.js.map
---
+++ src/docs/main.js
@@ -0,0 +1,5 @@
+import { Deposit } from "../../dist/AddFunds.js";
+import { WithdrawAmount } from "../../dist/WithdrawFunds.js";
+import { СheckBalance } from "../../dist/CheckBalance.js";
+import { clearTransaction } from "../../dist/ClearTransactions.js";
+//# sourceMappingURL=main.js.map
